c9dd4c67ba5cc369e17b9aaded87de8855b18d3d
pinky-banik <pinky.rani.banik@gmail.com>
2021-10-18 06:55:48 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBuwPRA-OrQx9rB6y3zroiIbyb6gYj-q5E",
+  authDomain: "coding-club-pro.firebaseapp.com",
+  databaseURL: "https://coding-club-pro-default-rtdb.firebaseio.com",
+  projectId: "coding-club-pro",
+  storageBucket: "coding-club-pro.appspot.com",
+  messagingSenderId: "543771675785",
+  appId: "1:543771675785:web:e8f37ac6cb738e375fca58",
+};
+
+export default firebaseConfig;
